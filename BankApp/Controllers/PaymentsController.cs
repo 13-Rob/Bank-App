b91fd91ef8b79f4a1891639a3bbc3886055a69e1
Roberto Gonzalez <rcgr.13@gmail.com>
2024-01-30 12:59:11 -0800

    Added Payments page and link on navbar

--- BankApp/Controllers/PaymentsController.cs
+++ BankApp/Controllers/PaymentsController.cs
@@ -1,10 +1,18 @@
-using BankApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using BankApp.Data;
+using BankApp.Models;
 
 public class PaymentsController : Controller {
-    private readonly MvcPaymentContext _context;
+    // private readonly MvcPaymentContext _context;
 
-    public PaymentsController(MvcPaymentContext context) {
-        _context = context;
+    // public PaymentsController(MvcPaymentContext context) {
+    //     _context = context;
+    // }
+
+    public IActionResult Index() {
+        return View();
     }
 }
