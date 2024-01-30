931c249c7094a7a0c513967dd3f4573820ed6dfb
Roberto Gonzalez <rcgr.13@gmail.com>
2024-01-30 13:12:57 -0800

    Created database and changed appsettings

--- BankApp/Data/PaymentContext.cs
+++ BankApp/Data/PaymentContext.cs
@@ -3,8 +3,8 @@ using BankApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BankApp.Data {
-    public class MvcPaymentContext : DbContext {
-        public MvcPaymentContext(DbContextOptions<MvcPaymentContext> options) : base(options) {
+    public class PaymentContext : DbContext {
+        public PaymentContext(DbContextOptions<PaymentContext> options) : base(options) {
         }
 
         public DbSet<Payment> Payment { get; set;}
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,4 +1,10 @@
+using BankApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PaymentContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("PaymentContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
