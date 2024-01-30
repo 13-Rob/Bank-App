00645df012cb15f96f36b2a2fced7785974b6712
Roberto Gonzalez <rcgr.13@gmail.com>
2024-01-30 15:04:35 -0800

    Added files for each CRUD view

--- BankApp/Controllers/PaymentsController.cs
+++ BankApp/Controllers/PaymentsController.cs
@@ -12,7 +12,34 @@ public class PaymentsController : Controller {
         _context = context;
     }
 
+    // GET: /Payments
     public async Task<IActionResult> Index() {
         return View(await _context.Payment.ToListAsync());
+    }
+
+    // GET: /Payments/Details/5
+    public async Task<IActionResult> Details(int? id) {
+        if (id == null) {
+            return NotFound();
+        }
+
+        var payment = await _context.Payment.FirstOrDefaultAsync(m => m.Id == id);
+        if (payment == null) {
+            return NotFound();
+        }
+        return View(payment);
+    }
+
+    // GET: /Payments/Edit/5
+    public async Task<IActionResult> Edit(int? id) {
+        if (id == null) {
+            return NotFound();
+        }
+
+        var payment = await _context.Payment.FirstOrDefaultAsync(m => m.Id == id);
+        if (payment == null) {
+            return NotFound();
+        }
+        return View(payment);
     }
 }
